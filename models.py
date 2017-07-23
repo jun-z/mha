69e398ae75aa274511121e43bef47eba04dbfed0
Jun Zhao <zhaojun7987@gmail.com>
2017-07-23 11:31:58 -0400

    update classification model

--- models.py
+++ models.py
@@ -1,28 +1,34 @@
 import torch
 import torch.nn as nn
-from modules import Linear, Encoder
+from modules import Encoder, PositionalEncoding
 
 
 class Classifier(nn.Module):
     def __init__(self,
-                 emb_size, vocab_size, label_size,
-                 n_l, n_h, d_m, d_h, d_k, d_v, dropout):
+                 layers, length,
+                 vocab_size, label_size,
+                 n_h, d_m, d_h, d_k, d_v, dropout):
 
         super(Classifier, self).__init__()
 
-        self.emb_size = emb_size
         self.vocab_size = vocab_size
         self.label_size = label_size
 
-        self.linear = Linear(d_m, label_size)
-        self.encoder = Encoder(n_h, d_m, d_h, d_k, d_v, dropout)
-        self.softmax = nn.Softmax()
+        self.enc = PositionalEncoding(d_m, length)
+        self.emb = nn.Embedding(vocab_size, d_m)
+        self.linear = nn.Linear(d_m, label_size, bias=False)
+
+        self.encoders = []
+        for _ in range(layers):
+            self.encoders.append(Encoder(n_h, d_m, d_h, d_k, d_v, dropout))
 
     def forward(self, _input):
-        _len = _input.size(1)
+        output = self.enc(self.emb(_input))
 
-        output, attn = self.encoder(_input)
-        output = self.linear(output).view(-1, self.label_size)
-        output = self.softmax(output).view(-1, _len, self.label_size)
+        for encoder in self.encoders:
+            output, attn = encoder(output)
 
-        return torch.mean(output, dim=-1)
+        output = torch.squeeze(torch.mean(output, dim=1))
+        output = self.linear(output)
+
+        return output
